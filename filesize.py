d5c6c752240086a0b338ec8b2497e4856fd880c6
sharma-charuu <134579479+sharma-charuu@users.noreply.github.com>
2023-05-25 19:06:24 +0530

    Add files via upload

--- filesize.py
+++ filesize.py
@@ -1,4 +1,8 @@
+#write a program to display the number of lines in the files and size of a file in bytes
 import os
 
-file_stat = os.stat('my_file.txt')
-print(file_stat.st_size)
+print("enter the name of file:")
+filename = input()
+sizeoffile = os.path.getsize(filename)
+print("\n size of given file(in bytes) is:")
+print(sizeoffile)
