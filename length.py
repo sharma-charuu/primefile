83aec739f0747c23a9c11cecb0e91a9f0ff95f43
sharma-charuu <134579479+sharma-charuu@users.noreply.github.com>
2023-05-26 11:52:16 +0530

    Delete length.py

--- length.py
+++
@@ -1,9 +0,0 @@
-#program to print even length words in a string
-f = []
-def length(s):
-    l = s.split(" ")
-    for i in l:
-        if len(i) % 2 == 0:
-            f.append(i)
-    print(' '.join(f))
-length("i am python")
